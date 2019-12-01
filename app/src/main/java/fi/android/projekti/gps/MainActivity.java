8d59b1b35179df3ecdaedffe2be4d97618b19abd
Miika Koppelo <t7komi01@students.oamk.fi>
2019-12-01 13:23:09 +0200

    second commit

--- app/src/main/java/fi/android/projekti/gps/MainActivity.java
+++ app/src/main/java/fi/android/projekti/gps/MainActivity.java
@@ -8,6 +8,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,35 +17,42 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button nappi;
+
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nappi = (Button) findViewById(R.id.button);
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+// Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
 
-        nappi.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-
-            public void onClick(View v) {
-                GPStracker g = new GPStracker(getApplicationContext());
-                Location l = g.getLocation();
-                TextView mTextView = (TextView) findViewById(R.id.Text);
-                if (l != null) {
-                    double lat = l.getLatitude();
-                    double lon = l.getLongitude();
-                    String lat2 = String.valueOf(lat);
-                    String lon2 = String.valueOf(lon);
-                    mTextView.setText("Latitude" + lat2 + "Longtitude" + lon);
-                }
             }
-        });
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("access denied");
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        Location sijainti = LocationManager.getLastKnownLocation
+
     }
 }
